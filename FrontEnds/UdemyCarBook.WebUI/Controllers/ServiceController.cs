c0e066e1276c76f1d94f908b1968c2dee586ffbf
EnesBatuhanHalac <halacenesbatuhan@gmail.com>
2024-10-20 00:47:01 +0300

    Front-End Kısmına Girildi.Bir Layout Oluşturuldu.About - Car - Contact Gibi Sayfalar yapıldı.Devam Edilecek..

---
+++ FrontEnds/UdemyCarBook.WebUI/Controllers/ServiceController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using UdemyCarBook.Dto.ServiceDtos;
+
+namespace UdemyCarBook.WebUI.Controllers
+{
+    public class ServiceController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ServiceController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var data = await client.GetAsync("https://localhost:7190/api/Services");
+            if (data.IsSuccessStatusCode)
+            {
+                var jsonString=await data.Content.ReadAsStringAsync();
+                var value = JsonConvert.DeserializeObject<List<ResultServiceDto>>(jsonString);
+                return View(value);
+            }
+            return View();
+        }
+    }
+}
